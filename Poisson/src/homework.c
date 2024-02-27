fff9813b24cd817ceab13a9a26333a0fc59fffe0
bdallemagne <brieuc.dallemagne@student.uclouvain.be>
2024-02-27 14:58:29 +0100

    oidfij

--- Poisson/src/homework.c
+++ Poisson/src/homework.c
@@ -26,22 +26,12 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
     femGeo* theGeometry = theProblem->geo;  
     femMesh* theEdges = theGeometry->theEdges; 
     int nBoundary = 0;
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for (int i = 0; i < theEdges->nElem; i++) {
+        if (theEdges->elem[i] == 0) {
+            nBoundary++;
+        }
+    }
 
     femDomain *theBoundary = malloc(sizeof(femDomain));
     theGeometry->nDomains++;
@@ -51,6 +41,10 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
     theBoundary->elem = malloc(nBoundary*sizeof(int));
     theBoundary->mesh = NULL;
     sprintf(theBoundary->name,"Boundary");
+
+    for(int i = 0; i < nBoundary; i++) {
+        theBoundary->elem[i] = 0;
+    }
  
 
 
@@ -97,44 +91,7 @@ void femPoissonSolve(femPoissonProblem *theProblem)
     double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
     int iElem,iInteg,iEdge,i,j,map[4];
     int nLocal = theMesh->nLocalNode;
-    //début copilot
-    double **A = theSystem->A;
-    double *B = theSystem->B;
-     
-    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
-        femPoissonLocal(theProblem,iElem,map,x,y);
-        for (i=0; i < nLocal; ++i) {
-            phi[i] = 0.0;
-            dphidxsi[i] = 0.0;
-            dphideta[i] = 0.0; 
-        }
-        for (iInteg=0; iInteg < theRule->n; iInteg++) {
-            double xsi = theRule->xsi[iInteg];
-            double eta = theRule->eta[iInteg];
-            double weight = theRule->weight[iInteg];
-            femDiscretePhi2(theSpace,xsi,eta,phi);
-            femDiscreteDphi2(theSpace,xsi,eta,dphidxsi,dphideta);
-            double dxdxsi = 0.0, dxdeta = 0.0, dydxsi = 0.0, dydeta = 0.0;
-            for (j=0; j < nLocal; ++j) {
-                dxdxsi += dphidxsi[j]*x[j];
-                dxdeta += dphideta[j]*x[j];
-                dydxsi += dphidxsi[j]*y[j];
-                dydeta += dphideta[j]*y[j]; 
-            }
-            double jacobian = dxdxsi*dydeta - dxdeta*dydxsi;
-            for (i=0; i < nLocal; ++i) {
-                dphidx[i] = (1.0/jacobian)*(dydeta*dphidxsi[i]-dydxsi*dphideta[i]);
-                dphidy[i] = (1.0/jacobian)*(-dxdeta*dphidxsi[i]+dxdxsi*dphideta[i]); 
-            }
-            for (i=0; i < nLocal; ++i) {
-                for (j=0; j < nLocal; ++j) {
-                    A[map[i]][map[j]] += (dphidx[i]*dphidx[j] + dphidy[i]*dphidy[j])*jacobian*weight; 
-                }
-                B[map[i]] += phi[i]*jacobian*weight;
-            } 
-        } 
-    }
-    //fin copilot
+
 
 
     
