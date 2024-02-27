a29f44989d75268e886a85397620301c6ea72e46
bdallemagne <brieuc.dallemagne@student.uclouvain.be>
2024-02-27 15:16:01 +0100

    copilot > nous

--- Poisson/src/homework.c
+++ Poisson/src/homework.c
@@ -69,10 +69,12 @@ void femPoissonFree(femPoissonProblem *theProblem)
 void femPoissonLocal(femPoissonProblem *theProblem, const int iElem, int *map, double *x, double *y)
 {
     femMesh *theMesh = theProblem->geo->theElements;
-    
-
-
-    
+    femDiscrete *theSpace = theProblem->space;
+    for (int i = 0; i < theMesh->nLocalNode; i++) {
+        map[i] = theMesh->elem[iElem*theMesh->nLocalNode+i];
+        x[i] = theMesh->nodes->X[map[i]];
+        y[i] = theMesh->nodes->Y[map[i]];
+    }
 }
 
 # endif
@@ -92,6 +94,73 @@ void femPoissonSolve(femPoissonProblem *theProblem)
     int iElem,iInteg,iEdge,i,j,map[4];
     int nLocal = theMesh->nLocalNode;
 
+    double **A = theSystem->A;
+    double *B = theSystem->B;
+    int size = theSystem->size;
+
+    for (int Elem; Elem < theMesh->nElem; Elem++) {
+        femPoissonLocal(theProblem,Elem,map,x,y);
+        for (int i = 0; i < theRule->n; i++) {
+            iInteg = i;
+            theSpace->phi2(theRule->xsi[iInteg],theRule->eta[iInteg],phi);
+            theSpace->dphi2dx(theRule->xsi[iInteg],theRule->eta[iInteg],dphidxsi,dphideta);
+            double dxdxsi = 0.0, dydxsi = 0.0, dxdeta = 0.0, dydeta = 0.0;
+            for (int j = 0; j < nLocal; j++) {
+                dxdxsi += dphidxsi[j]*x[j];
+                dydxsi += dphidxsi[j]*y[j];
+                dxdeta += dphideta[j]*x[j];
+                dydeta += dphideta[j]*y[j];
+            }
+            double jac = dxdxsi*dydeta - dydxsi*dxdeta;
+            double invJac = 1.0/jac;
+            double dphidx[4],dphidy[4];
+            for (int j = 0; j < nLocal; j++) {
+                dphidx[j] = invJac*(dphidxsi[j]*dydeta - dphideta[j]*dydxsi);
+                dphidy[j] = invJac*(dphideta[j]*dxdxsi - dphidxsi[j]*dxdeta);
+            }
+            double **a = malloc(nLocal*sizeof(double*));
+            for (int j = 0; j < nLocal; j++) {
+                a[j] = malloc(nLocal*sizeof(double));
+            }
+            double *b = malloc(nLocal*sizeof(double));
+            for (int j = 0; j < nLocal; j++) {
+                b[j] = 0.0;
+                for (int k = 0; k < nLocal; k++) {
+                    a[j][k] = 0.0;
+                }
+            }
+            for (int j = 0; j < nLocal; j++) {
+                for (int k = 0; k < nLocal; k++) {
+                    a[j][k] = a[j][k] + (dphidx[j]*dphidx[k] + dphidy[j]*dphidy[k])*jac*theRule->weight[iInteg];
+                }
+                b[j] = b[j] + 1.0*phi[j]*jac*theRule->weight[iInteg];
+            }
+            for (int j = 0; j < nLocal; j++) {
+                for (int k = 0; k < nLocal; k++) {
+                    A[map[j]][map[k]] = A[map[j]][map[k]] + a[j][k];
+                }
+                B[map[j]] = B[map[j]] + b[j];
+            }
+            for (int j = 0; j < nLocal; j++) {
+                free(a[j]);
+            }
+            free(a);
+            free(b);
+        }
+
+    }
+
+    for (int i = 0; i < theBoundary->nElem; i++) {
+        iElem = theBoundary->elem[i];
+        for (int j = 0; j < nLocal; j++) {
+            B[theMesh->elem[iElem*nLocal+j]] = 0.0;
+            for (int k = 0; k < size; k++) {
+                A[theMesh->elem[iElem*nLocal+j]][k] = 0.0;
+                A[k][theMesh->elem[iElem*nLocal+j]] = 0.0;
+            }
+            A[theMesh->elem[iElem*nLocal+j]][theMesh->elem[iElem*nLocal+j]] = 1.0;
+        }
+    }
 
 
     
