f5d2e4db4b8016a3a89852b0e5c88060357b6bd1
bdallemagne <brieuc.dallemagne@student.uclouvain.be>
2024-02-27 13:37:53 +0100

    avancement homework.c

--- Poisson/src/homework.c
+++ Poisson/src/homework.c
@@ -27,7 +27,20 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
     femMesh* theEdges = theGeometry->theEdges; 
     int nBoundary = 0;
     
-    //  A completer :-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     femDomain *theBoundary = malloc(sizeof(femDomain));
@@ -39,7 +52,7 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
     theBoundary->mesh = NULL;
     sprintf(theBoundary->name,"Boundary");
  
-    // A completer :-)
+
 
 }
     
@@ -49,7 +62,11 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
 void femPoissonFree(femPoissonProblem *theProblem)
 {
 
-    // A completer :-)
+    geoMeshFree(theProblem->geo);
+    femDiscreteFree(theProblem->space);
+    femIntegrationFree(theProblem->rule);
+    femFullSystemFree(theProblem->system);
+    free(theProblem);
 }
     
 # endif
@@ -59,8 +76,9 @@ void femPoissonLocal(femPoissonProblem *theProblem, const int iElem, int *map, d
 {
     femMesh *theMesh = theProblem->geo->theElements;
     
-    //  A completer :-)
 
+
+    
 }
 
 # endif
@@ -79,8 +97,45 @@ void femPoissonSolve(femPoissonProblem *theProblem)
     double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
     int iElem,iInteg,iEdge,i,j,map[4];
     int nLocal = theMesh->nLocalNode;
+    double **A = theSystem->A;
+    double *B = theSystem->B;
+     
+    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
+        femPoissonLocal(theProblem,iElem,map,x,y);
+        for (i=0; i < nLocal; ++i) {
+            phi[i] = 0.0;
+            dphidxsi[i] = 0.0;
+            dphideta[i] = 0.0; 
+        }
+        for (iInteg=0; iInteg < theRule->n; iInteg++) {
+            double xsi = theRule->xsi[iInteg];
+            double eta = theRule->eta[iInteg];
+            double weight = theRule->weight[iInteg];
+            femDiscretePhi2(theSpace,xsi,eta,phi);
+            femDiscreteDphi2(theSpace,xsi,eta,dphidxsi,dphideta);
+            double dxdxsi = 0.0, dxdeta = 0.0, dydxsi = 0.0, dydeta = 0.0;
+            for (j=0; j < nLocal; ++j) {
+                dxdxsi += dphidxsi[j]*x[j];
+                dxdeta += dphideta[j]*x[j];
+                dydxsi += dphidxsi[j]*y[j];
+                dydeta += dphideta[j]*y[j]; 
+            }
+            double jacobian = dxdxsi*dydeta - dxdeta*dydxsi;
+            for (i=0; i < nLocal; ++i) {
+                dphidx[i] = (1.0/jacobian)*(dydeta*dphidxsi[i]-dydxsi*dphideta[i]);
+                dphidy[i] = (1.0/jacobian)*(-dxdeta*dphidxsi[i]+dxdxsi*dphideta[i]); 
+            }
+            for (i=0; i < nLocal; ++i) {
+                for (j=0; j < nLocal; ++j) {
+                    A[map[i]][map[j]] += (dphidx[i]*dphidx[j] + dphidy[i]*dphidy[j])*jacobian*weight; 
+                }
+                B[map[i]] += phi[i]*jacobian*weight;
+            } 
+        } 
+    }
 
-    // A completer :-)
+
+    
 }
 
 # endif
