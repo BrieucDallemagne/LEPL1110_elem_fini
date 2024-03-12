e02dd94c6732b11d65e9e228d3a482fe514c34f1
bdallemagne <brieuc.dallemagne@student.uclouvain.be>
2024-03-12 15:08:22 +0100

    pls

--- groupe048-tirgel-bdallemagne/src/bash.sh
+++ groupe048-tirgel-bdallemagne/src/bash.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-read -p "voulez vous rebuild le projet ? [Y/n]:" reponse 
+read -p "voulez vous rebuild le projet ? [Y/n]:" reponse
 
 cd ../build
 
 if [ "$reponse" = "Y" ];
 then
+    rm -rf *
     cmake ..
     make
 fi 
