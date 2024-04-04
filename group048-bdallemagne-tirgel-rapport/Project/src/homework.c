f7e1151e9ecd98a81fe26b6c21c7c4a16de8c768
irgel <tom.irgel@student.uclouvain.be>
2024-04-04 21:46:16 +0200

    nice

--- group048-bdallemagne-tirgel-rapport/Project/src/homework.c
+++ group048-bdallemagne-tirgel-rapport/Project/src/homework.c
@@ -1,9 +1,9 @@
 #include "fem.h"
 
 // Il faut un fifrelin generaliser ce code.....
-//  (1) Ajouter l'axisymétrique !    (mandatory) // A faire
+//  (1) Ajouter l'axisymétrique !    (mandatory) // Ok (à vérifier)
 //  (2) Ajouter les conditions de Neumann !   (mandatory) // A faire
-//  (3) Ajouter les conditions en normal et tangentiel !   (strongly advised) // OK (à vérifier)
+//  (3) Ajouter les conditions en normal et tangentiel !   (strongly advised) // A faire (à vérifier)
 //  (4) Et remplacer le solveur plein par un truc un fifrelin plus subtil  (mandatory) // OK (à vérifier)
 
 void femElasticityAssembleElements(femProblem *theProblem) {
@@ -61,84 +61,28 @@ void femElasticityAssembleElements(femProblem *theProblem) {
       }
       for (i = 0; i < theSpace->n; i++) {
         for (j = 0; j < theSpace->n; j++) {
-          A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] + dphidy[i] * c * dphidy[j]) * jac * weight;
-          A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] + dphidy[i] * c * dphidx[j]) * jac * weight;
-          A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] + dphidx[i] * c * dphidy[j]) * jac * weight;
-          A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] + dphidx[i] * c * dphidx[j]) * jac * weight;
+          if (theProblem->planarStrainStress = AXISYM || PLANAR_STRESS){
+             A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] * x[i] + dphidy[i] * c * dphidy[j] * x[i] + dphidx[i] * b * phi[j] + phi[i] * (b * dphidx[j] + a * phi[j]/x[i])) * jac * weight;
+             A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] * x[i] + dphidy[i] * c * dphidx[j] * x[i] + phi[i] * b * dphidy[j]) * jac * weight;
+             A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] * x[i] + dphidx[i] * c * dphidy[j] * x[i] + dphidy[i] * b * phi[j]) * jac * weight;
+             A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] * x[i] + dphidx[i] * c * dphidx[j] * x[i]) * jac * weight;
+          }else{
+            A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] + dphidy[i] * c * dphidy[j]) * jac * weight;
+            A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] + dphidy[i] * c * dphidx[j]) * jac * weight;
+            A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] + dphidx[i] * c * dphidy[j]) * jac * weight;
+            A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] + dphidx[i] * c * dphidx[j]) * jac * weight;
+          }
+          
         }
       }
       for (i = 0; i < theSpace->n; i++) {
-        B[mapX[i]] += phi[i] * gx * rho * jac * weight;
-        B[mapY[i]] += phi[i] * gy * rho * jac * weight;
-      }
-    }
-  }
-}
-
-void femElasticityAssembleElements_axisymetric(femProblem *theProblem) {
-  femFullSystem *theSystem = theProblem->system;
-  femIntegration *theRule = theProblem->rule;
-  femDiscrete *theSpace = theProblem->space;
-  femGeo *theGeometry = theProblem->geometry;
-  femNodes *theNodes = theGeometry->theNodes;
-  femMesh *theMesh = theGeometry->theElements;
-  double x[4], y[4], phi[4], dphidxsi[4], dphideta[4], dphidx[4], dphidy[4];
-  int iElem, iInteg, iEdge, i, j, d, map[4], mapX[4], mapY[4];
-  int nLocal = theMesh->nLocalNode;
-  double a = theProblem->A;
-  double b = theProblem->B;
-  double c = theProblem->C;
-  double rho = theProblem->rho;
-  double gx = theProblem->gx;
-  double gy = theProblem->gy;
-  double **A = theSystem->A;
-  double *B = theSystem->B;
-
-  for (iElem = 0; iElem < theMesh->nElem; iElem++) {
-    for (j = 0; j < nLocal; j++) {
-      map[j] = theMesh->elem[iElem * nLocal + j];
-      mapX[j] = 2 * map[j];
-      mapY[j] = 2 * map[j] + 1;
-      x[j] = theNodes->X[map[j]];
-      y[j] = theNodes->Y[map[j]];
-    }
-
-    for (iInteg = 0; iInteg < theRule->n; iInteg++) {
-      double xsi = theRule->xsi[iInteg];
-      double eta = theRule->eta[iInteg];
-      double weight = theRule->weight[iInteg];
-      femDiscretePhi2(theSpace, xsi, eta, phi);
-      femDiscreteDphi2(theSpace, xsi, eta, dphidxsi, dphideta);
-
-      double dxdxsi = 0.0;
-      double dxdeta = 0.0;
-      double dydxsi = 0.0;
-      double dydeta = 0.0;
-      for (i = 0; i < theSpace->n; i++) {
-        dxdxsi += x[i] * dphidxsi[i];
-        dxdeta += x[i] * dphideta[i];
-        dydxsi += y[i] * dphidxsi[i];
-        dydeta += y[i] * dphideta[i];
-      }
-      double jac = dxdxsi * dydeta - dxdeta * dydxsi;
-      if (jac < 0.0) printf("Negative jacobian! Your mesh is oriented in reverse. The normals will be wrong\n");
-      jac = fabs(jac);
-
-      for (i = 0; i < theSpace->n; i++) {
-        dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / jac;
-        dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac;
-      }
-      for (i = 0; i < theSpace->n; i++) {
-        for (j = 0; j < theSpace->n; j++) {
-          A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] * x[i] + dphidy[i] * c * dphidy[j] * x[i] + dphidx[i] * b * phi[j] + phi[i] * (b * dphidx[j] + a * phi[j]/x[i])) * jac * weight;
-          A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] * x[i] + dphidy[i] * c * dphidx[j] * x[i] + phi[i] * b * dphidy[j]) * jac * weight;
-          A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] * x[i] + dphidx[i] * c * dphidy[j] * x[i] + dphidy[i] * b * phi[j]) * jac * weight;
-          A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] * x[i] + dphidx[i] * c * dphidx[j] * x[i]) * jac * weight;
+        if (theProblem->planarStrainStress = AXISYM || PLANAR_STRESS){
+          B[mapX[i]] += phi[i] * gx * rho * x[i] * jac * weight;
+          B[mapY[i]] += phi[i] * gy * rho * x[i] * jac * weight;
+        }else{
+          B[mapX[i]] += phi[i] * gx * rho * jac * weight;
+          B[mapY[i]] += phi[i] * gy * rho * jac * weight;
         }
-      }
-      for (i = 0; i < theSpace->n; i++) {
-        B[mapX[i]] += phi[i] * gx * rho * x[i] * jac * weight;
-        B[mapY[i]] += phi[i] * gy * rho * x[i] * jac * weight;
       }
     }
   }
@@ -264,7 +208,7 @@ double *femElasticitySolve(femProblem *theProblem) {
   femElasticityAssembleNeumann(theProblem);
   femElasticityApplyDirichlet(theProblem);
 
-  // Implémentation du solver CG (conjugate gradient) + CSR (compressed sparse row)
+  // Implémentation du solver CG (conjugate gradient) {fait} + CSR (compressed sparse row) {à implémenter}
 
   // double* soluce = femFullSystemEliminate(theProblem->system);
   double* soluce = cgSolver(theProblem->system);
