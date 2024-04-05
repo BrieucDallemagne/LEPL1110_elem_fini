6d5a89e3389515783d5fc9f44a31227e5f22b096
irgel <tom.irgel@student.uclouvain.be>
2024-04-05 15:01:39 +0200

    Version + archaïque mais ça marche en attendant

---
+++ group048-bdallemagne-tirgel-rapport/runner.sh
@@ -0,0 +1,11 @@
+cd /element_fini/LEPL1110_elem_fini/group048-bdallemagne-tirgel-rapport/ProjectPreProcessor/build
+make
+./myFem
+
+cd /element_fini/LEPL1110_elem_fini/group048-bdallemagne-tirgel-rapport/Project/build
+make
+./myFem
+
+cd /element_fini/LEPL1110_elem_fini/group048-bdallemagne-tirgel-rapport/ProjectPostProcessor/build
+make
+./myFem
